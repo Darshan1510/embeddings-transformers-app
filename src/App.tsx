70fcd07bf2209b346ea3d06c09f544e764e251c0
Darshan shah <darshan@mytradezone.com>
2024-08-07 16:46:41 -0700

    Components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import TextSentimentAnalysis from "./TextSentimentAnalysis";
-import TextSimilarityChecker from "./TextSimilarityChecker";
+import TextSentimentAnalysis from "./components/TextSentimentAnalysis";
+import TextSimilarityChecker from "./components/TextSimilarityChecker";
 
 function App() {
   return (
